24d7dcea78a20274b171eef8b7e0ae0ec1c7b57b
KaushikSawant <55449304+KaushikSawant@users.noreply.github.com>
2020-10-07 22:24:40 +0530

    Add files via upload

--- Q55.java
+++ Q55.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Q55 {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-  String str1="Rupali PAngare";
+  String str1="Kaushik sawant";
   String str3=sc.nextLine();
   String str2[]=str3.split(" ");
   int i=str2.length;
